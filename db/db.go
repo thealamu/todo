a761873f5b10bbcea489456a5d8f5f32ddd12be3
Faithfulness Alamu <vaguemail369@gmail.com>
2020-09-02 23:43:39 +0100

    Update db interface definition

--- db/db.go
+++ db/db.go
@@ -5,5 +5,5 @@ import "github.com/thealamu/todo/todo"
 // DB interafce holds common methods for db implementations
 type DB interface {
 	GetAllItems() []todo.Todo
-	GetSingleItem(id int) todo.Todo
+	GetSingleItem(id int) (todo.Todo, error)
 }

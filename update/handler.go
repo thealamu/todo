a22b9e7e7317b8aea312ddd149c15fda394d3210
Faithfulness Alamu <vaguemail369@gmail.com>
2020-09-02 12:47:57 +0100

    Register routes in update package

--- update/handler.go
+++ update/handler.go
@@ -1,1 +1,26 @@
 package update
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Handler handles all item creation
+type Handler struct{}
+
+// New returns a new Handler
+func New() *Handler {
+	return &Handler{}
+}
+
+// Register registers the endpoints in mux
+func (h *Handler) Register(mux *mux.Router) {
+	mux.HandleFunc("/todos/{id}", h.UpdateSingle).Methods(http.MethodPut)
+}
+
+// UpdateSingle updates a single to-do item
+func (h *Handler) UpdateSingle(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Updating Single")
+}

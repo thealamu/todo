85fce9db3ee313011c5f56d931e8b384f268bec3
Faithfulness Alamu <vaguemail369@gmail.com>
2020-09-02 23:33:43 +0100

    Create method to get single to-do item

--- db/db.go
+++ db/db.go
@@ -5,4 +5,5 @@ import "github.com/thealamu/todo/todo"
 // DB interafce holds common methods for db implementations
 type DB interface {
 	GetAllItems() []todo.Todo
+	GetSingleItem(id int) todo.Todo
 }

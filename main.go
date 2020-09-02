782b415382389536cde1e69168fbf83f29210bd8
Faithfulness Alamu <vaguemail369@gmail.com>
2020-09-02 12:55:14 +0100

    Delegate route registering in main

--- main.go
+++ main.go
@@ -6,6 +6,12 @@ import (
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/thealamu/todo/create"
+	"github.com/thealamu/todo/delete"
+	"github.com/thealamu/todo/retrieve"
+	"github.com/thealamu/todo/update"
 )
 
 func main() {
@@ -14,13 +20,24 @@ func main() {
 	if port == "" {
 		port = "1028"
 	}
+
+	// Build the router
+	router := mux.NewRouter()
+	// Delegate route registering
+	create.New().Register(router)
+	retrieve.New().Register(router)
+	update.New().Register(router)
+	delete.New().Register(router)
+
 	// Build the address to run on
 	srvAddr := net.JoinHostPort("", port)
 	srv := &http.Server{
 		Addr:         srvAddr,
+		Handler:      router,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 5 * time.Second,
 	}
+
 	// Start the server
 	err := srv.ListenAndServe()
 	if err != nil {

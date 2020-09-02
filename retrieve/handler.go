3ef6dc1f9fe8697453da30c63ff8f25f79a515cf
Faithfulness Alamu <vaguemail369@gmail.com>
2020-09-02 19:55:12 +0100

    Implement handler to get all to-do items

--- retrieve/handler.go
+++ retrieve/handler.go
@@ -1,6 +1,7 @@
 package retrieve
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -31,10 +32,27 @@ func (h *Handler) Register(mux *mux.Router) {
 
 // GetAll serves all to-do items
 func (h *Handler) GetAll(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Getting All")
+	h.logger.Println("Getting all to-do items")
+	items := h.db.GetAllItems()
+	h.logger.Printf("Returning %d to-do items\n", len(items))
+	// Write JSON of items to output
+	err := h.RespondJSON(w, items)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 // GetSingle serves a single to-do item
 func (h *Handler) GetSingle(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Getting Single")
+}
+
+// RespondJSON responds with data as JSON
+func (h *Handler) RespondJSON(w http.ResponseWriter, data ...interface{}) error {
+	coded, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	w.Write(coded)
+	return nil
 }

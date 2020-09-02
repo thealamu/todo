2684a52147590171b130141949299c7d7092ab61
Faithfulness Alamu <vaguemail369@gmail.com>
2020-09-02 19:43:34 +0100

    Implement getting all to-do items from memory

--- db/memdb.go
+++ db/memdb.go
@@ -1,9 +1,18 @@
 package db
 
+import "github.com/thealamu/todo/todo"
+
 // MemoryDB is an in-memory implementation of the DB interface
 type MemoryDB struct{}
+
+var inMemTodos []todo.Todo
 
 // NewInMem returns an in-memory DB
 func NewInMem() *MemoryDB {
 	return &MemoryDB{}
+}
+
+// GetAllItems returns all to-do items
+func (m *MemoryDB) GetAllItems() []todo.Todo {
+	return inMemTodos
 }

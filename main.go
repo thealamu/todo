98237d8444f9408c10ace8fc79d2ede2daa3c580
Faithfulness Alamu <vaguemail369@gmail.com>
2020-09-02 12:11:56 +0100

    Init the server in main

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"time"
+)
 
+func main() {
+	// We read port from the environment but default to 1028
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "1028"
+	}
+	// Build the address to run on
+	srvAddr := net.JoinHostPort("", port)
+	srv := &http.Server{
+		Addr:         srvAddr,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+	// Start the server
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }

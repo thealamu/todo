0abaed8f3210b43410dc4ee52ef2628185f33d94
Faithfulness Alamu <vaguemail369@gmail.com>
2020-09-02 12:40:36 +0100

    Register routes in retrieve package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/thealamu/todo
 
 go 1.14
+
+require github.com/gorilla/mux v1.8.0
--- retrieve/handler.go
+++ retrieve/handler.go
@@ -1,1 +1,32 @@
 package retrieve
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Handler handles all item creation
+type Handler struct{}
+
+// New returns a new Handler
+func New() *Handler {
+	return &Handler{}
+}
+
+// Register registers the endpoints in mux
+func (h *Handler) Register(mux *mux.Router) {
+	mux.HandleFunc("/todos", h.GetAll).Methods(http.MethodGet)
+	mux.HandleFunc("/todos/{id}", h.GetSingle).Methods(http.MethodGet)
+}
+
+// GetAll serves all to-do items
+func (h *Handler) GetAll(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Getting All")
+}
+
+// GetSingle serves a single to-do item
+func (h *Handler) GetSingle(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Getting Single")
+}

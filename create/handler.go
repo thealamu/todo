3534287c0f3a4ceccea90648cb74b5ac2edec074
Faithfulness Alamu <vaguemail369@gmail.com>
2020-09-02 12:44:39 +0100

    Register routes in create package

--- create/handler.go
+++ create/handler.go
@@ -1,1 +1,26 @@
 package create
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Handler handles all item creation
+type Handler struct{}
+
+// New returns a new Handler
+func New() *Handler {
+	return &Handler{}
+}
+
+// Register registers the endpoints in mux
+func (h *Handler) Register(mux *mux.Router) {
+	mux.HandleFunc("/todos", h.CreateSingle).Methods(http.MethodPost)
+}
+
+// CreateSingle creates a single to-do item
+func (h *Handler) CreateSingle(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Creating Single")
+}

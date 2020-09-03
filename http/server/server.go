0d74ba65404cc0f7dabfd620e069cc9691891803
Faithfulness Alamu <vaguemail369@gmail.com>
2020-09-03 09:15:40 +0100

    Move server into http package

--- http/server/server.go
+++ http/server/server.go
@@ -1,1 +1,16 @@
 package server
+
+import (
+	"net/http"
+	"time"
+)
+
+// New returns a newly configured http server
+func New(addr string, router http.Handler) *http.Server {
+	return &http.Server{
+		Addr:         addr,
+		Handler:      router,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+}
